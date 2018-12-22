38e35c6bc9ccdc570cfc124a4eca6e17298b2a9f
cnotv <contact@cnotv.xyz>
2018-12-22 12:07:06 +0100

    Add initial informations

---
+++ src/models/polygon.interface.ts
@@ -0,0 +1,7 @@
+export interface Polygon {
+  name: string;
+  population: number;
+  averageAge: number;
+  area: number;
+  perimeter: number;
+}
--- src/store.ts
+++ src/store.ts
@@ -3,9 +3,19 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+export const AgeColors = {
+  40: '#00cb09',
+  42: '#a4cb00',
+  43: '#cb8a00',
+  45: '#cb4f00',
+  50: '#cb0026',
+};
+
 export default new Vuex.Store({
   state: {
-
+    current: {
+      loc: [52.520008, 13.404954],
+    },
   },
   mutations: {
 
